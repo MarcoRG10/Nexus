2d871971c34ad7bb694ca7d5cda7e8da83f8c132
MarcoRG10 <juan.marcorg12@gmail.com>
2025-01-29 20:54:13 -0500

    actualizar estado ,para visualizar en pantalla completa

--- modules/home/components/ListState.jsx
+++ modules/home/components/ListState.jsx
@@ -1,53 +1,112 @@
-import React, { useState } from 'react';
-import { FlatList } from 'react-native';
-import State from './State.jsx';
+import React, { useState } from "react";
+import {
+  FlatList,
+  Modal,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import State from "./State";
+import Icon from "react-native-vector-icons/FontAwesome";
+export default function ListStates() {
+  const [estados] = useState([
+    {
+      id: 1,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStihKLR4QsliwDXi8x2pJx20dW0zfRKVAiaA&s",
+    },
+    {
+      id: 2,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQElbaePMJA_KW3V1R32-_UqA4YxC6TuYpxlg&s",
+    },
+    {
+      id: 3,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBioLvC31VJfcfkE4hfU05iLTNfE5o6oFKgA&s",
+    },
+  ]);
 
+  const [modalVisible, setModalVisible] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  export default function ListStates ()  {
-    const [estados, setEstados] = useState([
-      {id:1, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStihKLR4QsliwDXi8x2pJx20dW0zfRKVAiaA&s', seen: true },
-      {id:2, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQElbaePMJA_KW3V1R32-_UqA4YxC6TuYpxlg&s', seen: true },
-      {id:3, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBioLvC31VJfcfkE4hfU05iLTNfE5o6oFKgA&s', seen: true },
-      {id:4, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpj2elRrHLpPcNjxMfFdLA9zSoxiCbivEvHQ&s', seen: true },
-      {id:5, image: 'https://i0.wp.com/www.otero.uy/wp-content/uploads/2023/03/fotos-para-perfil-de-linkedin-hombre-2.jpg?fit=1080%2C1080&ssl=1', seen: true },
-      {id:6, image: 'https://media.glamour.mx/photos/65b092a43756393e0200c63b/master/w_1600%2Cc_limit/foto-de-perfil-artistica.jpg', seen: true },
-      {id:7, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREsFljbBIY4oH82Qb6WwOsA9vRO-Pcon2Uog&s', seen: true },
-      // ... más estados
-    ]);
-    const handleStatePress = (image) => {
-      console.log('Estado presionado', image);
+  const handleStatePress = (index) => {
+    setCurrentIndex(index);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  const goNext = () => {
+    if (currentIndex < estados.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      closeModal();
     }
-  
-    return (
+  };
+
+  const goPrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  return (
+    <View>
       <FlatList
         data={estados}
         horizontal
         showsHorizontalScrollIndicator={false}
-        renderItem={({ item }) => <State image={item.image} seen={item.seen} onPress={()=> handleStatePress(item.image)}/>}
-        keyExtractor={(item) => item.id}
+        renderItem={({ item, index }) => (
+          <State image={item.image} onPress={() => handleStatePress(index)} />
+        )}
+        keyExtractor={(item) => item.id.toString()}
       />
-      
-    );
-  };
-  
 
+      <Modal visible={modalVisible} transparent={true} animationType="fade">
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "black",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity
+            style={{ position: "absolute", top: 40, right: 20, zIndex: 1 }} 
+            onPress={closeModal}
+          >
+            <Icon name="times" size={30} color="white" />
+          </TouchableOpacity>
 
+          <View style={{ flex: 1, width: "100%" }}> 
+            <Image
+              source={{ uri: estados[currentIndex].image }}
+              style={{ width: "100%", height: "100%", resizeMode: "contain" }}
+            />
+          </View>
 
+          {currentIndex > 0 && (
+            <TouchableOpacity
+              style={{ position: "absolute", left: 20, top: "50%" }}
+              onPress={goPrev}
+            >
+              <Icon name="chevron-left" size={40} color="white" />{" "}
+            </TouchableOpacity>
+          )}
 
-
-
-
-{
-  /*
-  const images = [
-    require("../../../assets/cat-7779237_640.jpg"),
-    require("../../../assets/bird-8336436_640.jpg"),
-    require("../../../assets/dog-9190504_640.jpg"),
-    require("../../../assets/flycatcher-8864922_640.jpg"),
-    require("../../../assets/grey-wolf-8492789_640.png"),
-    require("../../../assets/sea-8445435_640.jpg"),
-    require("../../../assets/whisky-8443153_640.jpg"),
-  ];
-
-  */
+          {currentIndex < estados.length - 1 && (
+            <TouchableOpacity
+              style={{ position: "absolute", right: 20, top: "50%" }}
+              onPress={goNext}
+            >
+              <Icon name="chevron-right" size={40} color="white" />{" "}
+            </TouchableOpacity>
+          )}
+        </View>
+      </Modal>
+    </View>
+  );
 }
